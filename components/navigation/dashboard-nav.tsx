1d79216c68446dabb5ce9097f4f8b3fcfd924c26
rmthomas <rmthomas1998@gmail.com>
2025-04-24 22:10:14 -0400

    remove feedback button

--- components/navigation/dashboard-nav.tsx
+++ components/navigation/dashboard-nav.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import NextLink from "next/link";
-import { Button } from "@/components/ui/button";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Breadcrumbs } from "@/components/navigation/breadcrumbs";
 import { Suspense } from "react";
@@ -17,9 +16,9 @@ export const DashboardNav = () => {
           </Suspense>
         </div>
         <div className="flex items-center gap-4">
-          <Button size="sm" variant="outline">
-            Feedback
-          </Button>
+          {/*<Button size="sm" variant="outline">*/}
+          {/*  Feedback*/}
+          {/*</Button>*/}
           <NextLink
             href="/"
             className="text-muted-foreground hover:text-foreground text-sm transition-colors"
