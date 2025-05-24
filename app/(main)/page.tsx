defc204684566adbeb311a4e423ad1d6e836078d
rmthomas <rmthomas1998@gmail.com>
2025-05-24 17:26:52 -0400

    change grid opacity

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="absolute inset-0 z-[-1]">
         <div className="from-background to-background absolute inset-0 z-10 bg-gradient-to-b via-transparent"></div>
         <div className="from-background to-background absolute inset-0 z-10 bg-gradient-to-r via-transparent"></div>
-        <AnimatedGridPattern maxOpacity={0.1} className="z-[-1]" />
+        <AnimatedGridPattern maxOpacity={0.07} className="z-[-1]" />
       </div>
       <Hero />
     </div>
